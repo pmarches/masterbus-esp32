8008d92106bd397277c7eb98b3d3725ffb0e95a1
Philippe Marchesseault <pmarches@pop-os.localdomain>
2020-06-18 09:34:48 -0400

    Before creating a MastervoltMessage class

--- main/include/mvParser.hpp
+++ main/include/mvParser.hpp
@@ -3,22 +3,102 @@
 
 #include <string>
 #include <map>
+#include <mastervoltMessage.h>
+
+class MastervoltPacket;
+class MastervoltDeviceKind;
+
+class MastervoltAttributeKind {
+public:
+	enum MastervoltEncoding {
+		FLOAT_ENCODING,
+		STRING_ENCODING,
+	};
+	enum MastervoltDataType {
+		UNKNOWN_DT,
+		SIMPLE_FLOAT_DT,
+		DATE_DT,
+		TIME_DT,
+		TEXT_DT,
+		BYTES_DT,
+		REQUEST_DT,
+		LABEL_DT
+	};
+	MastervoltAttributeKind(const MastervoltDeviceKind* parent, uint16_t attributeKind, std::string textDescription, MastervoltEncoding encoding, MastervoltDataType dataType)
+		: MastervoltAttributeKind(attributeKind, textDescription, encoding, dataType){
+	}
+	MastervoltAttributeKind(uint16_t attributeKind, std::string textDescription, MastervoltEncoding encoding, MastervoltDataType dataType);
+	MastervoltDeviceKind* parent;
+	uint16_t attributeKind;
+	std::string textDescription;
+	MastervoltEncoding encoding; //How is the underlying data encoded? With floats? Strings?
+	MastervoltDataType dataType; //What does the data look like? A date? A float? Text?
+
+	std::string packetValueToString(MastervoltPacket& packet) const;
+	std::string toString() const;
+};
+
+class MastervoltDeviceKind {
+public:
+	MastervoltDeviceKind(uint32_t deviceKind);
+	void addAttribute(MastervoltAttributeKind att);
+	std::string toString() const;
+	uint32_t deviceKind;
+	std::map<uint16_t, MastervoltAttributeKind> attributeKind;
+};
+
+class MastervoltDictionary {
+protected:
+	MastervoltDictionary();
+	static MastervoltDictionary* instance;
+	std::map<uint32_t, MastervoltDeviceKind> deviceKind;
+	const MastervoltDeviceKind UNKNOWN_DEVICE_KIND;
+	const MastervoltAttributeKind UNKNOWN_ATTRIBUTE_KIND;
+public:
+	static const uint32_t DCSHUNT_KIND;
+	static const uint32_t INVERTER_KIND;
+	static const uint32_t BATTERY_CHARGER_KIND;
+	static const uint32_t MASTERVIEW_KIND;
+
+	static const uint32_t DCSHUNT_CANID;
+	static const uint16_t DCSHUNT_BATTERY_PERCENTAGE;
+	static const uint16_t DCSHUNT_BATTERY_VOLTS;
+	static const uint16_t DCSHUNT_BATTERY_AMPS;
+	static const uint16_t DCSHUNT_BATTERY_AMPS_CONSUMED;
+	static const uint16_t DCSHUNT_BATTERY_TEMPERATURE;
+	static const uint16_t DCSHUNT_DATE;
+	static const uint16_t DCSHUNT_TIME;
+
+	static const uint32_t INVERTER_CANID;
+	static const uint16_t INVERTER_DC_VOLTAGE_IN;
+	static const uint16_t INVERTER_DC_AMPS_IN;
+	static const uint16_t INVERTER_AC_AMPS_OUT;
+	static const uint16_t INVERTER_STATE;
+
+	static const uint16_t DCSHUNT_DAYOFMONTH;
+	static const uint16_t DCSHUNT_MONTHOFYEAR;
+	static const uint16_t DCSHUNT_YEAROFCALENDAR;
+
+
+	static MastervoltDictionary* getInstance() { if(instance==nullptr) instance=new MastervoltDictionary(); return instance;}
+	const MastervoltAttributeKind& resolveAttribute(uint32_t canId, uint16_t attributeId);
+};
 
 class MastervoltPacket {
 public:
-	MastervoltPacket(): canId(0), dataType(0), attributeId(0), valueType(UNKNOWN){
+	MastervoltPacket(): canId(0), attributeId(0), dataType(0), valueType(UNKNOWN), floatValue(0){
 	}
 //	uint8_t getGroupNumber();
 //	uint8_t getItemNumber();
 
 	std::string getLabel();
 	float getFloatValue();
-	uint32_t getIntValue();
-	std::string getUnit();
 
 	uint32_t canId;
+	uint8_t attributeId; //What is the value about? Battery volts? Battery Amps? Time? Date? These attribute Id overlap across different device type
+
 	uint8_t dataType;
-	uint8_t attributeId;
+
 	enum MastervoltPacketType {
 		UNKNOWN,
 		REQUEST,
@@ -26,18 +106,17 @@ public:
 		LABEL,
 	} valueType;
 
-	union {
-		float floatValue;
-	};
+	float floatValue;
 	std::string labelContent;
 	std::string unparsed;
 
+	bool isRequest(){ return (canId&0x10000000) != 0; }
+	uint32_t getDeviceUniqueId() { return canId&0x7FFF0000; }
 	void dump();
 };
 
 
 class MvParser {
-	static const std::map<uint16_t, MastervoltPacket::MastervoltPacketType> attributeToTypeMap;
 	static std::map<uint16_t, MastervoltPacket::MastervoltPacketType> populateAttributeIdToTypeMap();
 public:
 	bool parse(uint32_t canId, std::string& stringToParse, MastervoltPacket* mvPacket);
@@ -46,38 +125,6 @@ public:
 	float parseValueAsFloat();
 	std::string stringToParse;
 	MastervoltPacket* mvPacket;
-};
-
-class MastervoltDictionary {
-	std::map<uint16_t, std::string> attributeIdToLabelMap;
-	MastervoltDictionary();
-	static MastervoltDictionary* instance;
-public:
-	//	attributeIdToLabelMap[0x00]="batteryPercentage";
-	//	attributeIdToLabelMap[0x02]="batteryCurrent";
-	//	attributeIdToLabelMap[0x03]="batteryAmpHourConsumed";
-	//	attributeIdToLabelMap[0x05]="batteryTemperature";
-	//	attributeIdToLabelMap[0x06]="massCombiDCVoltageIn";
-	//	attributeIdToLabelMap[0x07]="massCombiDCCurrentIn";
-	//	attributeIdToLabelMap[0x0b]="massCombiACAmpsOut";
-	//	attributeIdToLabelMap[0x14]="massCombiInverterState?";
-
-	static const uint32_t DCSHUNT_CANID;
-	static const uint16_t DCSHUNT_BATTERY_PERCENTAGE;
-	static const uint16_t DCSHUNT_BATTERY_VOLTS;
-	static const uint16_t DCSHUNT_BATTERY_AMPS;
-	static const uint16_t DCSHUNT_BATTERY_AMPS_CONSUMED;
-	static const uint16_t DCSHUNT_BATTERY_TEMPERATURE;
-
-	static const uint32_t INVERTER_CANID;
-	static const uint16_t INVERTER_DC_VOLTAGE_IN;
-	static const uint16_t INVERTER_DC_AMPS_IN;
-	static const uint16_t INVERTER_AC_AMPS_OUT;
-	static const uint16_t INVERTER_STATE;
-
-	static MastervoltDictionary* getInstance() { if(instance==nullptr) instance=new MastervoltDictionary(); return instance;}
-	std::string resolveCanIdToLabel(long int canId);
-	std::string resolveAttributeIdToLabel(uint16_t attId);
 };
 
 
--- main/mvParser.cpp
+++ main/mvParser.cpp
@@ -5,6 +5,7 @@
 
 #include <sstream>
 #include <iomanip>
+#include <iterator>
 
 
 void MastervoltPacket::dump(){
@@ -13,18 +14,6 @@ void MastervoltPacket::dump(){
 	ESP_LOGD(__FUNCTION__, "dataType=%x", this->dataType);
 	ESP_LOGD(__FUNCTION__, "floatValue=%f", this->floatValue);
 }
-
-const std::map<uint16_t, MastervoltPacket::MastervoltPacketType> MvParser::attributeToTypeMap = {
-	{0x00, MastervoltPacket::MastervoltPacketType::FLOAT},
-	{0x02, MastervoltPacket::MastervoltPacketType::FLOAT},
-	{0x03, MastervoltPacket::MastervoltPacketType::FLOAT},
-	{0x05, MastervoltPacket::MastervoltPacketType::FLOAT},
-	{0x06, MastervoltPacket::MastervoltPacketType::FLOAT},
-	{0x07, MastervoltPacket::MastervoltPacketType::FLOAT},
-	{0x0b, MastervoltPacket::MastervoltPacketType::FLOAT},
-	{0x14, MastervoltPacket::MastervoltPacketType::FLOAT},
-};
-
 
 bool MvParser::parse(uint32_t canId, std::string& stringToParse, MastervoltPacket* mvPacket){
 	this->stringToParse=stringToParse;
@@ -48,14 +37,12 @@ bool MvParser::parse(uint32_t canId, std::string& stringToParse, MastervoltPacke
 		this->mvPacket->valueType=MastervoltPacket::LABEL;
 		return parseString();
 	}
-	else if(attributeToTypeMap.find(mvPacket->attributeId)!=attributeToTypeMap.end()){
+	else if(this->mvPacket->dataType==0x00){
 		this->mvPacket->valueType=MastervoltPacket::FLOAT;
 		this->mvPacket->floatValue=parseValueAsFloat();
 	}
 	else {
-		std::string attributeLabel=MastervoltDictionary::getInstance()->resolveAttributeIdToLabel(this->mvPacket->attributeId);
 		mvPacket->unparsed=stringToParse;
-		ESP_LOGW(__FUNCTION__, "UKNX	%s	0x%x	0x%x", attributeLabel.c_str(), this->mvPacket->canId, this->mvPacket->attributeId);
 	}
 
 	return true;
@@ -75,15 +62,101 @@ bool MvParser::parseString(){
 	return true;
 }
 
+MastervoltDeviceKind::MastervoltDeviceKind(uint32_t deviceKind) : deviceKind(deviceKind) {
+	const uint32_t DEVICE_KIND_MASK=0x1000FFFF;
+	if((deviceKind&DEVICE_KIND_MASK)!=0){
+		ESP_LOGE(__FUNCTION__, "Don't pass in a canId here, pass in the device type, which is 0x%X", DEVICE_KIND_MASK);
+	}
+}
+
+std::string MastervoltDeviceKind::toString() const{
+	return "DeviceKindGoesHere";
+}
+
+MastervoltAttributeKind::MastervoltAttributeKind(uint16_t attributeKind, std::string textDescription, MastervoltEncoding encoding, MastervoltDataType dataType):
+		parent(nullptr), attributeKind(attributeKind), textDescription(textDescription), encoding(encoding), dataType(dataType) {
+}
+
+void MastervoltDeviceKind::addAttribute(MastervoltAttributeKind att){
+	att.parent=this;
+	attributeKind.insert(std::pair<uint16_t, MastervoltAttributeKind>(att.attributeKind, att));
+}
+
+std::string MastervoltAttributeKind::packetValueToString(MastervoltPacket& packet) const {
+	std::stringstream ss;
+	if(encoding==FLOAT_ENCODING){
+		if(dataType==TIME_DT){
+			//Day of month = float%32, Year=float/32/13, Month=decimal remainder of year*13
+			//840528.00==16/06/2020
+			//840944.00==16/05/2021
+			//840943.00==15/06/2021
+			//840527.00==15/06/2020
+			//840559.00==15/07/2020
+			//840591.00==15/08/2020
+			//832207.00==15/06/2000 : The masterview interface does not allow for less than year 2000
+			//832034.00==02/01/2000 : The interface fails to show the date if we set the date to 01/01/2000. This is probably the default
+
+			uint32_t secondElapsedSinceMidnight=packet.floatValue;
+			uint16_t hour=secondElapsedSinceMidnight/60/60;
+			secondElapsedSinceMidnight-=(hour*60*60);
+			uint16_t minute=secondElapsedSinceMidnight/60;
+			secondElapsedSinceMidnight-=(minute*60);
+			uint16_t second=secondElapsedSinceMidnight;
+			ss<<hour<<":"<<minute<<":"<<second;
+		}
+		else if(dataType==DATE_DT){
+			uint32_t nbDaysZeroBC=packet.floatValue;
+			uint16_t day=nbDaysZeroBC%32;
+			nbDaysZeroBC-=day;
+			float yearAndFraction=nbDaysZeroBC/416.0; //32 days *13 months=416
+			uint16_t year=yearAndFraction;
+			yearAndFraction-=year;
+			uint16_t month=yearAndFraction*13;
+			ss<<day<<"/"<<month<<"/"<<year;
+		}
+		else{
+			ss.setf( std::ios::fixed, std::ios::floatfield );
+			ss.precision(2);
+			ss << packet.floatValue;
+		}
+	}
+	else if(dataType==REQUEST_DT){
+		ss << 'R';
+	}
+	else if(dataType==UNKNOWN_DT){
+		ss<< std::setw(2) << std::setfill('0') << std::hex << std::uppercase;
+		std::copy(packet.unparsed.begin(), packet.unparsed.end(), std::ostream_iterator<uint32_t>(ss, "_"));
+		return ss.str();
+	}
+	else if(dataType==LABEL_DT){
+		ss << "Label "<<packet.labelContent;
+	}
+
+	return ss.str();
+}
+
+std::string MastervoltAttributeKind::toString() const {
+	return textDescription;
+}
 
 MastervoltDictionary* MastervoltDictionary::instance=nullptr;
 
+//TODO These values are dependant on the device type. The device type is encoded in the CANId, but some bits are unique per device.
+//TODO Refactor these attributes into a per device organization
 const uint32_t MastervoltDictionary::DCSHUNT_CANID=0x086f1297;
 const uint16_t MastervoltDictionary::DCSHUNT_BATTERY_PERCENTAGE=0x00;
 const uint16_t MastervoltDictionary::DCSHUNT_BATTERY_VOLTS=0x01;
 const uint16_t MastervoltDictionary::DCSHUNT_BATTERY_AMPS=0x02;
 const uint16_t MastervoltDictionary::DCSHUNT_BATTERY_AMPS_CONSUMED=0x03;
 const uint16_t MastervoltDictionary::DCSHUNT_BATTERY_TEMPERATURE=0x05;
+const uint16_t MastervoltDictionary::DCSHUNT_DATE=0x0a; //The number of days since day 0
+const uint16_t MastervoltDictionary::DCSHUNT_TIME=0x09; //The float represents the number of seconds elapsed since 00:00:00
+
+//0x0bef1297 Is the canid for configuring the date/time. Perhaps the masterview id?
+const uint16_t MastervoltDictionary::DCSHUNT_DAYOFMONTH=0x13;
+const uint16_t MastervoltDictionary::DCSHUNT_MONTHOFYEAR=0x14;
+const uint16_t MastervoltDictionary::DCSHUNT_YEAROFCALENDAR=0x15;
+
 
 const uint32_t MastervoltDictionary::INVERTER_CANID=0x083af412;
 const uint16_t MastervoltDictionary::INVERTER_DC_VOLTAGE_IN=0x06;
@@ -91,47 +164,40 @@ const uint16_t MastervoltDictionary::INVERTER_DC_AMPS_IN=0x07;
 const uint16_t MastervoltDictionary::INVERTER_AC_AMPS_OUT=0x0b;
 const uint16_t MastervoltDictionary::INVERTER_STATE=0x14; //1.0=On 0.0=Off
 
-MastervoltDictionary::MastervoltDictionary(){
-//	attributeIdToLabelMap[0x00]="batteryPercentage";
-//	attributeIdToLabelMap[0x02]="batteryCurrent";
-//	attributeIdToLabelMap[0x03]="batteryAmpHourConsumed";
-//	attributeIdToLabelMap[0x05]="batteryTemperature";
-//	attributeIdToLabelMap[0x06]="massCombiDCVoltageIn";
-//	attributeIdToLabelMap[0x07]="massCombiDCCurrentIn";
-//	attributeIdToLabelMap[0x0b]="massCombiACAmpsOut";
-//	attributeIdToLabelMap[0x14]="massCombiInverterState?";
+const uint32_t MastervoltDictionary::DCSHUNT_KIND=666;
+const uint32_t MastervoltDictionary::INVERTER_KIND=666;
+const uint32_t MastervoltDictionary::BATTERY_CHARGER_KIND=666;
+const uint32_t MastervoltDictionary::MASTERVIEW_KIND=666;
+
+MastervoltDictionary::MastervoltDictionary() : UNKNOWN_DEVICE_KIND(0), UNKNOWN_ATTRIBUTE_KIND(&UNKNOWN_DEVICE_KIND, MastervoltAttributeKind::UNKNOWN_DT, "Unknown", MastervoltAttributeKind::STRING_ENCODING, MastervoltAttributeKind::BYTES_DT) {
+	MastervoltDeviceKind dcShuntDeviceKind(DCSHUNT_KIND);
+	deviceKind.insert(std::pair<uint32_t, MastervoltDeviceKind>(DCSHUNT_KIND, dcShuntDeviceKind));
+	dcShuntDeviceKind.addAttribute(MastervoltAttributeKind(DCSHUNT_BATTERY_PERCENTAGE, "BatteryPercentage", MastervoltAttributeKind::FLOAT_ENCODING, MastervoltAttributeKind::SIMPLE_FLOAT_DT));
+	dcShuntDeviceKind.addAttribute(MastervoltAttributeKind(DCSHUNT_BATTERY_VOLTS, "BatteryVolts", MastervoltAttributeKind::FLOAT_ENCODING, MastervoltAttributeKind::SIMPLE_FLOAT_DT));
+	dcShuntDeviceKind.addAttribute(MastervoltAttributeKind(DCSHUNT_BATTERY_AMPS, "BatteryAmps", MastervoltAttributeKind::FLOAT_ENCODING, MastervoltAttributeKind::SIMPLE_FLOAT_DT));
+	dcShuntDeviceKind.addAttribute(MastervoltAttributeKind(DCSHUNT_BATTERY_AMPS_CONSUMED, "BatteryAmpsConsumed", MastervoltAttributeKind::FLOAT_ENCODING, MastervoltAttributeKind::SIMPLE_FLOAT_DT));
+	dcShuntDeviceKind.addAttribute(MastervoltAttributeKind(DCSHUNT_BATTERY_TEMPERATURE, "BatteryTemperature", MastervoltAttributeKind::FLOAT_ENCODING, MastervoltAttributeKind::SIMPLE_FLOAT_DT));
+	dcShuntDeviceKind.addAttribute(MastervoltAttributeKind(DCSHUNT_DATE, "Date", MastervoltAttributeKind::FLOAT_ENCODING, MastervoltAttributeKind::DATE_DT));
+	dcShuntDeviceKind.addAttribute(MastervoltAttributeKind(DCSHUNT_TIME, "Time", MastervoltAttributeKind::FLOAT_ENCODING, MastervoltAttributeKind::TIME_DT));
+
+	MastervoltDeviceKind inverterDeviceKind(INVERTER_KIND);
+	deviceKind.insert(std::pair<uint32_t, MastervoltDeviceKind>(INVERTER_KIND, inverterDeviceKind));
+	inverterDeviceKind.addAttribute(MastervoltAttributeKind(INVERTER_AC_AMPS_OUT, "ACAmpsOut", MastervoltAttributeKind::FLOAT_ENCODING, MastervoltAttributeKind::SIMPLE_FLOAT_DT));
+	inverterDeviceKind.addAttribute(MastervoltAttributeKind(INVERTER_DC_AMPS_IN, "DCAmpsIn", MastervoltAttributeKind::FLOAT_ENCODING, MastervoltAttributeKind::SIMPLE_FLOAT_DT));
+	inverterDeviceKind.addAttribute(MastervoltAttributeKind(INVERTER_DC_VOLTAGE_IN, "DCVoltsIn", MastervoltAttributeKind::FLOAT_ENCODING, MastervoltAttributeKind::SIMPLE_FLOAT_DT));
+	inverterDeviceKind.addAttribute(MastervoltAttributeKind(INVERTER_STATE, "InverterState", MastervoltAttributeKind::FLOAT_ENCODING, MastervoltAttributeKind::SIMPLE_FLOAT_DT));
 }
 
-std::string MastervoltDictionary::resolveCanIdToLabel(long int canId){
-//	if(canId==0x86f1297){
-//		return "dcShunt";
-//	}
-//	else if(canId==0x083af412){
-//		return "massCombi";
-//	}
-//	else if(canId==0x063af412){
-//		return "massCombiPowerState";
-//	}
-//	else
-	{
-		std::stringstream ss;
-		ss << "0x";
-		ss << std::hex;
-		ss << std::setw(8) << std::setfill('0') << canId;
-		return ss.str();
+const MastervoltAttributeKind& MastervoltDictionary::resolveAttribute(uint32_t canId, uint16_t attributeId){
+	uint32_t deviceKindId=canId&0x0000FFFF;
+	std::map<uint32_t, MastervoltDeviceKind>::iterator foundDeviceKind=deviceKind.find(deviceKindId);
+	if(foundDeviceKind==deviceKind.end()){
+		return UNKNOWN_ATTRIBUTE_KIND;
 	}
+	std::map<uint16_t, MastervoltAttributeKind>::iterator foundAttributeKind=foundDeviceKind->second.attributeKind.find(attributeId);
+	if(foundAttributeKind==foundDeviceKind->second.attributeKind.end()){
+		return UNKNOWN_ATTRIBUTE_KIND;
+	}
+	return foundAttributeKind->second;
 }
 
-std::string MastervoltDictionary::resolveAttributeIdToLabel(uint16_t attId){
-	auto attFound=attributeIdToLabelMap.find(attId);
-	if(attFound!=attributeIdToLabelMap.end()){
-		return attFound->second;
-	}
-	else {
-		std::stringstream ss;
-		ss << "0x";
-		ss << std::hex;
-		ss << std::setw(2) << std::setfill('0') << attId;
-		return ss.str();
-	}
-}
